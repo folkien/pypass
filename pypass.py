f6352e0ca7b8c3abda9a4d858029e009b1abf3c9
folkien <folkus@gmail.com>
2014-11-18 12:30:44 +0100

    Obsluga argumentow.

--- pypass.py
+++ pypass.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python
 
-import os, random
+import os, random, argparse, time
 
-file    = "password.pas"
-keyfile = "keyfile.key"
+parser = argparse.ArgumentParser()
+parser.add_argument("-i", "--inputfile", 	type=str, 		 required=True)
+parser.add_argument("-c", "--crypt", 	action='store_true', required=False)
+parser.add_argument("-d", "--decrypt", 	action='store_true', required=False)
+args = parser.parse_args()
+
+random.seed(time.time())
 
 def generateKey(length):
     key = ""
@@ -13,5 +18,11 @@ def generateKey(length):
     return key
 
 print generateKey(128)
+
+myfile = open(args.inputfile, 'r')
+if myfile.is_opened:
+		print "ok"
+else:
+		print "wrong"
 
 
