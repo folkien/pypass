ed2427e301c1f584a6123663c077a18969f3fb91
folkien <folkus@gmail.com>
2014-11-19 10:25:23 +0100

    Wersja 0.5 prawie finalna.

--- pypass.py
+++ pypass.py
@@ -1,21 +1,14 @@
 #!/usr/bin/python
 import os, random, argparse, time, sys
 
-#Key is generated from below chars.
-letters     = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'x', 'y', 'z', 'q', 'w', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'R', 'S', 'T', 'Y', 'X', 'Z', 'Q', 'W']
-numbers     = [ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' ]
-signs       = [ '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '=', '[', ']', ';', ':', '<', '>', '?', '.', ',' ]
-
-keys_dir 	= "/home/spasz/python/pypass/keys/"
-key_length	= 4096 
-key_signs       = letters + numbers + signs
-
+data = ""
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-i", "--inputfile", 	type=str, 		 required=True)
-parser.add_argument("-c", "--crypt", 	action='store_true', required=False)
-parser.add_argument("-d", "--decrypt", 	action='store_true', required=False)
-parser.add_argument("-n", "--newkey", 	action='store_true', required=False)
+parser.add_argument("-i", "--inputfile", 	type=str, 		 		required=False)
+parser.add_argument("-o", "--outputfile", 	type=str, 		 		required=False)
+parser.add_argument("-k", "--keyfile", 		type=str, 		 		required=False)
+parser.add_argument("-c", "--crypt", 		action='store_true', 	required=False)
+parser.add_argument("-n", "--newkey", 		type=int, 				required=False)
 args = parser.parse_args()
 
 random.seed(time.time())
@@ -23,48 +16,68 @@ random.seed(time.time())
 def generateKey(length):
     key = []
     for i in range(length):
-        key.append( key_signs[ random.randint(0,len(key_signs)-1) ] )
+        key.append(random.randint(0,255))
     return key
 
 def cipher(text,key):
 	key_length  = len(key)
 	text_length = len(text)
 	for i in range(text_length):
-			text[i] = chr(ord(text[i]) ^ ord(key[i % key_length]))
+			text[i] = chr(ord(text[i]) ^ key[i % key_length] )
 	return text
 
+def saveKey(filename,key):
+	kfile = open(filename, "w")
+	for i in range( len(key) ):
+			kfile.write( format(key[i], '02x') )
+	kfile.close()
 
-try:
-	myfile = open(args.inputfile, 'r')
-except:
-	print "This file doesn't exist."
-	sys.exit(0)
-
-
-data = list(myfile.read())
-
-#Sprawdzamy czy istnieje klucz
-if os.path.exists(keys_dir + args.inputfile + ".key"):
-	keyfile = open( keys_dir + args.inputfile + ".key", "r")
-	key = list(keyfile.read())
-	keyfile.close()
-	#Odszyfrowujemy plik
-	data = cipher(data,key)
-
-#wyswietlamy plik
-sys.stdout.write("".join(data))
+def loadKey(filename):
+	kfile = open(filename, "r")
+	data = kfile.read()
+	kfile.close()
+	i = 0
+	key = []
+	while ( i < len(data) ):
+			key.append(int(data[i]+data[i+1],16))
+			i += 2
+	return key
 
 #tworzymy nowy klucz
 if args.newkey:
-    key = generateKey(key_length)
-    keyfile = open( keys_dir + args.inputfile + ".key", "w")
-    keyfile.write("".join(key))
-    keyfile.close()
+	key = generateKey(args.newkey)
+	saveKey(args.keyfile, key)
+	print "Wygenerowano nowy klucz."
 
-#szyfrujemy ponownie
-data = cipher(data,key)
+#Otwieramy plik do szyfrowania
+if args.inputfile:
+		if os.path.exists(args.inputfile):
+			#otwieramy klucz
+			myfile = open(args.inputfile, 'r')
+			data = list(myfile.read())
+			myfile.close()
+		else:
+			print "File to encryption doesn't exist."
+			sys.exit(1)
 
-#zapisujemy plik
-myfile = open(args.inputfile, 'w')
-myfile.write("".join(data))
-myfile.close()
+#Szyfrujemy plik
+if args.crypt:
+	#Sprawdzamy czy istnieje klucz
+	if os.path.exists(args.keyfile):
+		#otwieramy klucz
+		key = loadKey(args.keyfile)
+	else:
+		print "Keyfile doesn't exists."
+		sys.exit(2)
+	#szyfrujemy plik
+	data = cipher(data,key)
+
+if args.outputfile:
+	#zapisujemy plik
+	myfile = open(args.outputfile, 'w')
+	myfile.write("".join(data))
+	myfile.close()
+else:
+	#wyswietlamy plik
+	sys.stdout.write("".join(data))
+
