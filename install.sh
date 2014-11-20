343f94dd556928191d445ed23a39fb601da6b54f
folkien <folkus@gmail.com>
2014-11-20 12:22:35 +0100

    Poprawka do skryptu instalacyjnego.

--- install.sh
+++ install.sh
@@ -4,3 +4,4 @@ cd /usr/bin
 sudo ln -s $SCRIPTDIR/pypass.py pypass 
 sudo ln -s $SCRIPTDIR/hasla.sh hasla
 sudo ln -s $SCRIPTDIR/hasla-edit.sh hasla-edit 
+sudo ln -s $SCRIPTDIR/hasla-add.sh hasla-add
